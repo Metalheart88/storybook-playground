1dee9a671dbf20404ae261b01425519cbc7fcf81
Greg Domanin <gregorydomanin@gmail.com>
2021-02-20 02:04:13 -0600

    Added first components to test with Storybook

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import Input from './Input'
+
+export default {
+  title: 'Form/Input',
+  component: Input,
+}
+
+export const Small = () => <Input size={'small'} placeholder={'Small size'} />
+export const Medium = () => (
+  <Input size={'medium'} placeholder={'Medium size'} />
+)
+export const Big = () => <Input size={'large'} placeholder={'Large size'} />
