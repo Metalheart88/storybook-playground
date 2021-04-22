75d391076029e412ad20d7fd0567d10fb109bf4e
Greg Domanin <gregorydomanin@gmail.com>
2021-04-22 00:46:22 -0500

    Added actions to button story

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Button } from '@chakra-ui/core'
+import { action, actions } from '@storybook/addon-actions'
 
-export default {
+const ChakraButton = {
   title: 'Chakra/Button',
   component: Button,
 }
 
-export const Success = () => <Button variantColor='green'>Success</Button>
-export const Danger = () => <Button variantColor='red'>Danger</Button>
+export default ChakraButton
+
+const Template = args => <Button {...args} />
+
+export const Success = Template.bind({})
+Success.args = {
+  variantColor: 'green',
+  children: 'Success',
+  onClick: action('Click handler'),
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  variantColor: 'red',
+  children: 'Danger',
+  ...actions('onClick', 'onMouseOver'),
+}
